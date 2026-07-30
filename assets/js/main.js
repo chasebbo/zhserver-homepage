69698211f3be8250450a01bdf82a276eab6be410
chasebbo <sebastian_bork@web.de>
2026-07-30 08:01:27 +0200

    Mobiles Hamburger-Menü hinzugefügt

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,3 +147,27 @@ if (guestbookEntries) {
     loadGuestbookEntries();
 
 }
+
+// ================= MOBILE MENU =================
+
+const menuToggle = document.querySelector("#menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+if(menuToggle){
+
+    menuToggle.addEventListener("click",()=>{
+
+        navMenu.classList.toggle("active");
+
+    });
+
+}
+document.querySelectorAll(".nav-menu a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navMenu.classList.remove("active");
+
+    });
+
+});
