9f3ad4a63d33d7c7b1fad63578090dbac757d594
chasebbo <sebastian_bork@web.de>
2026-07-30 14:26:49 +0200

    Remove duplicate Supabase URL declaration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 
 // ================= SUPABASE =================
 
-const SUPABASE_URL = "https://yawadxzeyyrozmlrokun.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhd2FkeHpleXlyb3ptbHJva3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzQ4MTIsImV4cCI6MjEwMDkxMDgxMn0.B53O3gHURnfxUkVGKaZJ5ssx27Bj9FNMU70Yn85tfxE";
 
 // ================= GÄSTEBUCH SPEICHERN =================
