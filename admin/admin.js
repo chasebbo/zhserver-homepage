47a236e45b8b83ddf0c9cc10fb85e7894a8c4035
chasebbo <sebastian_bork@web.de>
2026-07-30 11:10:45 +0200

    Admin Login korrigiert

--- admin/admin.js
+++ admin/admin.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://yawadxzeyyrozmlrokun.supabase.co";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhd2FkeHpleXlyb3ptbHJva3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzQ4MTIsImV4cCI6MjEwMDkxMDgxMn0.B53O3gHURnfxUkVGKaZJ5ssx27Bj9FNMU70Yn85tfxE";
 
-const supabase = window.supabase.createClient(
+const sb = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
@@ -16,7 +16,7 @@ loginBtn.addEventListener("click", async () => {
 
     loginMessage.textContent = "Anmeldung läuft...";
 
-    const { error } = await supabase.auth.signInWithPassword({
+    const { error } = await sb.auth.signInWithPassword({
 
         email: email.value,
         password: password.value
