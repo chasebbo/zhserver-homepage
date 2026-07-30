012b03a8dd1e93ddcd70e69b2c95b39951b39a72
chasebbo <sebastian_bork@web.de>
2026-07-30 13:38:50 +0200

    Fix duplicate Supabase declarations

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "DEINE_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "DEIN_ANON_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhd2FkeHpleXlyb3ptbHJva3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzQ4MTIsImV4cCI6MjEwMDkxMDgxMn0.B53O3gHURnfxUkVGKaZJ5ssx27Bj9FNMU70Yn85tfxE";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
