5e5562cd00f0b80428761ae636a441dc9573b5c4
chasebbo <sebastian_bork@web.de>
2026-07-30 14:44:27 +0200

    Fix Supabase client initialization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 const SUPABASE_URL = "https://yawadxzeyyrozmlrokun.supabase.co";
-const SUPABASE_KEY = "const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhd2FkeHpleXlyb3ptbHJva3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzQ4MTIsImV4cCI6MjEwMDkxMDgxMn0.B53O3gHURnfxUkVGKaZJ5ssx27Bj9FNMU70Yn85tfxE";
-";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhd2FkeHpleXlyb3ptbHJva3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzQ4MTIsImV4cCI6MjEwMDkxMDgxMn0.B53O3gHURnfxUkVGKaZJ5ssx27Bj9FNMU70Yn85tfxE";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
